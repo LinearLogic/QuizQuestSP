f663a1c3fcff5c1fa5a53e98f0ed7e4c94c4cd32
Jared <silkrawnstudios@gmail.com>
2012-10-27 13:53:42 -0400

    Work on Entity tile parsing and loading system

--- src/ss/linearlogic/quizquest/Map.java
+++ src/ss/linearlogic/quizquest/Map.java
@@ -13,22 +13,27 @@ import org.newdawn.slick.util.ResourceLoader;
 public class Map {
 	private static int map[][];
 	
+	//Dimensions of the map
 	private static int width;
 	private static int height;
 	
-	private static HashMap<Integer, Texture> textures = new HashMap();
+	private static HashMap<Integer, Texture> textures = new HashMap(); //Hashmap of all the textures
 	
-	private static int tileSize;
+	private static int tileSize; // Size of the tile
 	
 	public Map(String filename) {}
 	
+	//Initialize the map class
 	public static void Initialize(String filename) {
 		LoadMapFile(filename);
 	}
 	
+	//Render the current map
 	public static void Render() {
 		for (int x = 0; x < width; ++x) {
 			for (int y = 0; y < height; ++y) {
+				//2 Layers of rendering, the base layer being the grass and the top layer being the objects like doors and walls, etc.
+				
 				Renderer.RenderTexturedRectangle(x * tileSize, y * tileSize, tileSize, tileSize, textures.get(0));
 				if (map[x][y] == 0)
 					continue;
@@ -38,6 +43,7 @@ public class Map {
 		}
 	}
 	
+	//Add a texture to the textures hashmap
 	public static void AddTexture(String filename, int GID) {
 		Texture texture = null;
 		
@@ -51,6 +57,7 @@ public class Map {
 		else System.out.println("Texture is null, unable to add to array");
 	}
 	
+	//Load map file
 	private static void LoadMapFile(String filename) { 
 		Scanner textReader = null;
 		try {
@@ -73,5 +80,23 @@ public class Map {
 		}
 		
 		textReader.close();
+	}
+	
+	//Use method to parse the file and get the objects out, so you know where the objects are
+	private static void CreateObjects() {
+		for (int x = 0; x < width; ++x) {
+			for (int y = 0; y < height; ++y) {
+				switch (map[x][y]) {
+				case 0:
+					break;
+				case 1: //This is a door object
+					break;
+				case 2: //This is a wall object
+					break;
+				case 3: //This is a roof object (not really needed)
+					break;
+				}
+			}
+		}
 	}
 }
