3edadbe1c005b9220eec1c23e0ca78bdd4238001
Jared <silkrawnstudios@gmail.com>
2012-10-27 20:04:48 -0400

    Add world pixel location getters and Javadocs to
    Entity class

--- src/ss/linearlogic/quizquest/entity/Entity.java
+++ src/ss/linearlogic/quizquest/entity/Entity.java
@@ -7,13 +7,37 @@ import ss.linearlogic.quizquest.Map;
  */
 public class Entity {
 
-	/*
-	 * NOTE: completion of this class is postponed until the location management system has been fleshed out to the extent necessary.
+	/**
+	 * Entity type name ("Grass", "Door", "Enemy", etc.)
 	 */
-	private String type; // "Player"; "Enemy"; "Wall"; "Door"
-	private int typeID; // 1 --> Door; 2 --> Wall; 3 --> Roof; 4 --> Enemy
-	private int x; // x-coord of entity location
-	private int y; // y-coord of entity location
+	private String type;
+	
+	/**
+	 * Entity type ID (0 -> Grass, 1 -> Floor, 2 -> Wall, 3 -> Door, 4 -> Enemy)
+	 */
+	private int typeID;
+	
+	/**
+	 * X-coord of entity location in the entityMap 2D array
+	 */
+	private int x;
+	
+	/**
+	 * Y-coord of entity location in the entityMap 2D array
+	 */
+	private int y;
+	
+	/**
+	 * Represents distance in pixels from the x-coordinate of the top lefthand corner of the world
+	 * to the x-coordinate of the top lefthand corner of the Entity tile
+	 */
+	private int world_coordinates_x = Map.getTileSize() * x;
+	
+	/**
+	 * Represents distance in pixels from the y-coordinate of the top lefthand corner of the world
+	 * to the y-coordinate of the top lefthand corner of the Entity tile
+	 */
+	private int world_coordinates_y = Map.getTileSize() * y;
 	
 		public Entity(int typeID, int x, int y) {
 			this.typeID = typeID;
@@ -44,16 +68,46 @@ public class Entity {
 		Map.removeEntity(this.x, this.y);
 	}
 	
-	//-------[getters and setters]-------//
+	//--- Getters and setters ---//
+	/**
+	 * @return The {@link #type} name of the entity
+	 */
 	public String getType() { return type; }
 	
+	/**
+	 * @return The {@link #typeID}
+	 */
 	public int getTypeID() { return typeID; }
 	
+	/**
+	 * @return The {@link #x}-coordinate of the entity in the entityMap 2D array
+	 */
 	public int getX() { return this.x; }
 	
+	/**
+	 * Sets the entity's {@link #x}-coordinate in the entityMap 2D array to the supplied value
+	 * @param x
+	 */
 	public void setX(int x) { this.x = x; }
 	
+	/**
+	 * @return The {@link #y}-coordinate of the entity in the entityMap 2D array
+	 */
 	public int getY() { return this.y; }
 	
+	/**
+	 * Sets the entity's {@link #y}-coordinate in the entityMap 2D array to the supplied value
+	 * @param y
+	 */
 	public void setY(int y) { this.y = y; }
+	
+	/**
+	 * @return The {@link #world_coordinates_x} value of the entity
+	 */
+	public int getWorldX() { return this.world_coordinates_x; }
+	
+	/**
+	 * @return The {@link #world_coordinates_y} value of the entity
+	 */
+	public int getWorldY() { return this.world_coordinates_y; }
 }
